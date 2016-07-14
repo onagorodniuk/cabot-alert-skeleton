664686b6abc8413e545f280642bf4c826e58507f
onagorodniuk <gagarinq9@gmail.com>
2016-07-14 11:26:23 +0300

    Update plugin.py

--- cabot_alert_skeleton/plugin.py
+++ cabot_alert_skeleton/plugin.py
@@ -1,29 +1,14 @@
-from cabot.plugins.models import AlertPlugin
-from django import forms
-from os import environ as env
+from django.db import models
+from cabot.cabotapp.alert import AlertPlugin, AlertPluginUserData
 
-from logging import getLogger
-logger = getLogger(__name__)
-
-class SkeletonAlertUserSettingsForm(forms.Form):
-    favorite_bone = forms.CharField(max_length=100)
-
-class SkeletonAlertPlugin(AlertPlugin):
-    name = "Skeleton"
-    slug = "cabot_alert_skeleton"
-    author = "Jonathan Balls"
-    version = "0.0.1"
-    font_icon = "fa fa-code"
-
-    user_config_form = SkeletonAlertUserSettingsForm
+class SnsAlert(AlertPlugin):
+    name = "Sns"
+    author = "Oleksandr Nagorodniuk"
 
     def send_alert(self, service, users, duty_officers):
-        calcium_level = env.get('CALCIUM_LEVEL') 
-        message = service.get_status_message()
-        for u in users:
-            logger.info('{} - This is bad for your {}.'.format(
-                message,
-                u.cabot_alert_skeleton_settings.favorite_bone))
+        """Implement your send_alert functionality here."""
+        return
 
-        return True
-
+class SesAlertUserData(AlertPluginUserData):
+    name = "Ses Plugin"
+    favorite_bone = models.CharField(max_length=50, blank=True)
